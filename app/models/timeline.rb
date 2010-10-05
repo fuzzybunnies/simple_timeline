19ee29a9f859d220899c07a8804bb07e410757b5
john <fuzzybunnies@gmail.com>
2010-10-04 22:31:25 -0400

    added the description field

--- app/models/timeline.rb
+++ app/models/timeline.rb
@@ -2,8 +2,10 @@ class Timeline
   include Mongoid::Document
 
   field :name
+  field :description
 
   embeds_many :events, :default => []
 
   validates_presence_of :name
+  validates_presence_of :description
 end
