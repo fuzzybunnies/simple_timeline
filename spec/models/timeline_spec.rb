ca5b140072ebf4779534ee0658bc58f3b88b5715
john <fuzzybunnies@gmail.com>
2010-10-04 22:30:44 -0400

    tests for the timeline model

--- spec/models/timeline_spec.rb
+++ spec/models/timeline_spec.rb
@@ -2,9 +2,19 @@ require 'spec_helper'
 
 describe Timeline do
 
-  it "should not be valid without a name" do
+  it "should not be valid without a name and description" do
     t = Timeline.make
     t.should_not be_valid
+    t.name = "test"
+    t.should_not be_valid
+    t.name = nil
+    t.description = "test"
+    t.should_not be_valid
+  end
+
+  it "should be valid with a name and a description" do
+    t = Timeline.make(:name => 'test', :description => 'test')
+    t.should be_valid
   end
 
   it "should embed events" do
