aeaa350c27036ca1c3a4aabd57acc8547526e2e6
john <fuzzybunnies@gmail.com>
2010-10-10 21:04:31 -0400

    added resources for timeline events

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 TimelineMaker::Application.routes.draw do
-  resources :timelines
+  resources :timelines do
+    resources :events, :only => [:new, :create]
+  end
 
   root :to => "timelines#index"
 
