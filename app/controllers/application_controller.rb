ab7eeef8594811dc7f705a1b3e2f027caefe0595
john <fuzzybunnies@gmail.com>
2010-10-10 21:09:33 -0400

    added properties for the current timeline context for session storage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+
+  def current_timeline=(timeline)
+    unless timeline.nil?
+      session["timeline_id"] = timeline.id
+    end
+  end
+  def current_timeline
+    unless session["timeline_id"].blank?
+      return Timeline.find(session["timeline_id"])
+    end
+    nil
+  end
 end
