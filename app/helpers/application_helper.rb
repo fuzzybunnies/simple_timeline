dd33ff16c79048c11f9cd9e43ccfc77d03512463
john <fuzzybunnies@gmail.com>
2010-10-10 21:11:28 -0400

    added method to return the add event link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def add_event_link
+    if controller.current_timeline.nil?
+      flash[:information] = "Please select a timeline to add an event to."
+      return link_to 'Add Event', timelines_path
+    else
+      return link_to 'Add Event', new_timeline_event_path(controller.current_timeline)
+    end
+  end
 end
